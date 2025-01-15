be4ca92de2373eeb28688df791392e591d6ad551
Michael (a.k.a) Mz3ro <michaelsaraivatoniolo@gmail.com>
2025-01-15 13:56:21 -0300

    Update Instalador OpenSUSE VScode

--- OS_Scripts/openSUSE/vscode_opensuse.sh
+++ OS_Scripts/openSUSE/vscode_opensuse.sh
@@ -1,18 +1,5 @@
-#!/bin/sh
-
-
-sudo zypper in whois nmap gnome-calendar opi touchegg
-
-sudo flatpak install flameshot -y
-sudo flatpak install spotify -y
-sudo flatpak install io.github.shiftey.Desktop -y
-sudo flatpak install com.mattjakeman.ExtensionManager -y
-sudo flatpak install org.telegram.desktop -y
-sudo flatpak install com.github.donadigo.appeditor -y
-
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
 
 sudo zypper ref
 sudo zypper in code
-
