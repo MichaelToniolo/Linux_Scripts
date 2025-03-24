6bd801f0d86783f9384fc2f0c4a8c78a28f84e80
Michael (a.k.a) Mz3ro <michaelsaraivatoniolo@gmail.com>
2025-03-24 11:23:37 -0300

    Create Debian_AfterInstall.sh

---
+++ OS_Scripts/Debian_Scripts/Debian_AfterInstall.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sudo apt update
+sudo sudo apt install flatpak whois nmap thunderbird gnome-software-plugin-flatpak -y
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+sudo apt purge gnome-games -y
+sudo remove openvpn -y
+sudo flatpak install flameshot -y
+sudo flatpak install spotify -y
+sudo flatpak install io.github.shiftey.Desktop -y
+sudo flatpak install com.mattjakeman.ExtensionManager -y
+sudo flatpak install org.telegram.desktop -y
+sudo flatpak install com.github.donadigo.appeditor -y
+
+cd ~/Downloads
+wget https://launchpad.net/ubuntu/+source/openvpn/2.5.5-1ubuntu3/+build/23491549/+files/openvpn_2.5.5-1ubuntu3_amd64.deb && sudo dpkg -i openvpn_2.5.5-1ubuntu3_amd64.deb
+sudo apt-mark hold openvpn -y
+sudo apt install network-manager-openvpn-gnome -y
+
+
+
+
+
+
