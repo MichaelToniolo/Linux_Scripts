a176e2da8eccbc47023d37421f7535f1f9982a0a
Michael (a.k.a) Mz3ro <michaelsaraivatoniolo@gmail.com>
2025-04-01 09:42:45 -0300

    Create Gnome_Desktop.sh

---
+++ OS_Scripts/ParrotOS/Gnome_Desktop.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install libspa-0.2-modules=0.3.65-3+deb12u1
+sudo apt install pipewire=0.3.65-3+deb12u1 libpipewire-0.3-0=0.3.65-3+deb12u1
+sudo apt update && sudo apt install parrot-desktop-gnome
+
+
