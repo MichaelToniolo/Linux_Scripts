674c411e02f1d7224068391e474c8832c5baa78f
Michael (a.k.a) Mz3ro <37878223+MichaelToniolo@users.noreply.github.com>
2025-04-23 14:26:36 -0300

    Atualização de Pós Instalação Ubuntu

--- OS_Scripts/Ubuntu_Scripts/Ubuntu_InstalaApps.sh
+++ OS_Scripts/Ubuntu_Scripts/Ubuntu_InstalaApps.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-sudo apt update
-sudo apt install gnome-tweaks gnome-software gnome-software-plugin-flatpak thunderbird flameshot nmap whois
 
+sudo apt install gnome-tweaks gnome-software-plugin-flatpak nmap whois flatpak -y
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo apt remove openvpn -y
+
+sudo flatpak install flameshot -y
+sudo flatpak install io.github.shiftey.Desktop -y
+sudo flatpak install com.mattjakeman.ExtensionManager -y
+sudo flatpak install org.telegram.desktop -y
+sudo flatpak install com.github.donadigo.appeditor -y
+flatpak permission-set screenshot screenshot org.flameshot.Flameshot yes
+flatpak permission-set screenshot screenshot flameshot yes
+
+cd /home/michael/Downloads
+wget https://launchpad.net/ubuntu/+source/openvpn/2.5.5-1ubuntu3/+build/23491549/+files/openvpn_2.5.5-1ubuntu3_amd64.deb && sudo dpkg -i openvpn_2.5.5-1ubuntu3_amd64.deb
+sudo apt-mark hold openvpn
+sudo apt install network-manager-openvpn-gnome -y
