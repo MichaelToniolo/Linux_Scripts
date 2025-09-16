294e1c73d616da43965ca1831b310c08639f4245
Michael (a.k.a) Mz3ro <37878223+MichaelToniolo@users.noreply.github.com>
2025-09-16 14:39:15 -0300

    Removendo comando de Instalação do Code

--- OS_Scripts/openSUSE/openSUSE_After.sh
+++ OS_Scripts/openSUSE/openSUSE_After.sh
@@ -10,10 +10,5 @@ sudo flatpak install com.mattjakeman.ExtensionManager -y
 sudo flatpak install org.telegram.desktop -y
 sudo flatpak install com.github.donadigo.appeditor -y
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
-
-sudo zypper ref
-sudo zypper in code
 
 flatpak permission-set screenshot screenshot flameshot yes && flatpak permission-set screenshot screenshot org.flameshot.Flameshot yes
